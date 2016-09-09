e4f952e60ebd759cc2bafa80e52bfeab1fc5cdc5
SaintlyVi <wiebke.toussaint@gmail.com>
2016-09-09 08:51:17 +0200

    modified new_user and search for user

--- ckanclasses.py
+++ ckanclasses.py
@@ -68,51 +68,74 @@ def show(name, datatype=None, apikey=apikey):
 
   
 def search(query, datatype=None, apikey=apikey):
-    """Takes a string input and returns search results matching existing CKAN objects (organisation, dataset or resource).
+    """Takes a string input and returns search results matching existing CKAN objects (organisation, dataset, resource, user).
     
     Arguments:
     query -- a single search term 
              For 'resource' datatypes this defaults to searching the resource name. 
              Additional arguments can be added using the following syntax: 'query1 field2:query2'
              Valid resource fields can be used as field terms for the search.
-    datatype -- organisation, project, dataset or resource (default None)
+    datatype -- organisation, project, dataset, resource, user (default None)
     apikey -- valid CKAN API key (default None)
               Private datasets will only be shown to authorised API keys 
     """
-    d = [] #create empty list object to contain API call results
-    if datatype == None: datatype = input('Are you looking for an organisation, a project, a dataset or a resource?\n\n').lower().strip()    
+# Create empty list object to contain API call results    
+    d = [] 
+# Get datatype if not specified by user 
+    if datatype == None: datatype = input('Are you looking for an organisation, project, dataset, resource or user?\n\n').lower().strip()    
 
+# Check for datatype organisation or project
     if datatype == 'organisation' or datatype == 'project':   
         try:
             d = RemoteCKAN(url, apikey).action.organization_autocomplete(q=query)
         except Exception:
             print('No organisation or project exists for this search term')
+
+# Check for dataytpe dataset
     elif datatype == 'dataset': 
         try:
             d = RemoteCKAN(url, apikey).action.package_autocomplete(q=query)
             for i in d: i.pop('match_displayed') #remove match_displayed:value pair from dicts
         except Exception:
             print('No dataset exists for this search term')
+
+# Check for datatype resource
     elif datatype == 'resource': 
         try:
             query = ''.join(['name:', query])
-            d = RemoteCKAN(url, apikey).action.resource_search(query=query.split(" "))['results']
+            resources = RemoteCKAN(url, apikey).action.resource_search(query=query.split(" "))['results']
+            for r in range(len(resources)):
+                d.append({k : resources[r][k] for k in ('description','format','id','last_modified','name','package_id','revision_id')})
         except Exception:
-            print('No resource exists for this search term')        
+            print('No resource exists for this search term')
+
+# Check for datatype user
+    elif datatype == 'user': 
+        try:
+            users = RemoteCKAN(url, apikey).action.user_list(q=query)
+            for u in range(len(users)):
+                d.append({'username':users[u]['name'], 'fullname':users[u]['fullname']})
+        except Exception:
+            print('Cannot find user for this search term') 
     else:
         print('Please try a different search query and type a valid data type. This can be an organisation, project, dataset or resource.')
-   
-    if len(d) > 0: return pd.DataFrame(d).T
+
+# Format results dataframe to be returned   
+    if len(d[0]) > len(d) > 0: 
+        return(pd.DataFrame(d).T)
+    elif len(d) > 0:
+        return(pd.DataFrame(d))
         
-def new_user(self, username, email, fullname, apikey=apikey):
-        if self.name is not None:
-            d = dict(id=self.name, name=username, email=email, password='I love data', fullname=fullname)
-        else:
-            raise ValueError('You have not specified the required properties') 
-        try:
-            RemoteCKAN(url, apikey).action.user_create(**d)
-        except NotAuthorized:
-            print('Denied. Check your apikey.')
+def new_user(username, email=None, fullname=None, apikey=apikey):
+    while email is None:            
+        email = input('Enter new user\'s email address:\n') or None
+    while fullname is None:
+        fullname = input('Enter new user\'s full name:\n') or None            
+    d = dict(name=username.lower().strip(), email=email, password='I love data', fullname=fullname)
+    try:
+        RemoteCKAN(url, apikey).action.user_create(**d)
+    except NotAuthorized:
+        print('Denied. Check your apikey.')
 
 
 class CkanBase(object):
@@ -172,6 +195,11 @@ class Organisation(CkanBase):
             print('Denied. Check your apikey.') # print 'denied' if call not authorised
            
     def add_user(self, username, role, apikey=apikey):
+        try:
+            RemoteCKAN(url, apikey).action.user_search(id=username) ######CHECK API CALL!!
+        except None:
+            print('User does not exist. Let\'s create her.')
+            new_user(username)
         if self.name is not None:
             d = dict(id=self.name, username=username, role=role)
         else:
